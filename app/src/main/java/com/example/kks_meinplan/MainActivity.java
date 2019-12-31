d34ddb60796b7a09db3e6519249f2e6e16ccfe92
hd45 <hdi@users.noreply.github.com>
2019-12-31 16:01:33 +0100

    KKS App commit

--- app/src/main/java/com/example/kks_meinplan/MainActivity.java
+++ app/src/main/java/com/example/kks_meinplan/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.kks_meinplan;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -22,8 +24,6 @@ import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
-    private SwitchCompat switch1, switch2;
-    MenuItem nav_logout;
 
 
     @Override
@@ -32,11 +32,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
-        switch1 = findViewById(R.id.switch1);
-        switch2 = findViewById(R.id.switch2);
+
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigatonView = findViewById(R.id.nav_view);
-        nav_logout = findViewById(R.id.nav_logout);
 
         setSupportActionBar(toolbar);
 
@@ -53,64 +51,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             navigatonView.setCheckedItem(R.id.nav_vertretungsplan);
         }
 
-
-        SharedPreferences preferences = getSharedPreferences("save", MODE_PRIVATE);
-        switch1.setChecked(preferences.getBoolean("value", true));
-
-
-        switch1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (switch1.isChecked()) {
-
-                    SharedPreferences.Editor editor = getSharedPreferences("save",
-                            MODE_PRIVATE).edit();
-                    editor.putBoolean("value", true);
-                    editor.apply();
-                    switch1.setChecked(true);
-                } else {
-                    SharedPreferences.Editor editor = getSharedPreferences("save",
-                            MODE_PRIVATE).edit();
-                    editor.putBoolean("value", false);
-                    editor.apply();
-                    switch1.setChecked(false);
-                }
-            }
-
-
-        });
-
-        SharedPreferences preferences2 = getSharedPreferences("save2", MODE_PRIVATE);
-        switch2.setChecked(preferences2.getBoolean("value2", true));
-
-        switch2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (switch2.isChecked()) {
-
-                    SharedPreferences.Editor editor = getSharedPreferences("save2",
-                            MODE_PRIVATE).edit();
-                    editor.putBoolean("value2", true);
-                    editor.apply();
-                    switch2.setChecked(true);
-                } else {
-                    SharedPreferences.Editor editor = getSharedPreferences("save2",
-                            MODE_PRIVATE).edit();
-                    editor.putBoolean("value2", false);
-                    editor.apply();
-                    switch2.setChecked(false);
-                }
-            }
-
-
-        });
-
-        nav_logout.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem menuItem) {
-                return false;
-            }
-        });
 
     }
 
@@ -149,13 +89,40 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
 
-    //todo logout Anzeige ob man scih wirklich abmelden will
-    //todo willst du die App wirklich verlassen
+        } else {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle("Exit Application?");
+            alertDialogBuilder
+                    .setMessage("Click yes to exit!")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    moveTaskToBack(true);
+                                    android.os.Process.killProcess(android.os.Process.myPid());
+                                    System.exit(1);
+                                }
+                            })
+
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
+
+
+        //todo 1 logout alert dialog
+        //todo 2 switches Code ist richtig
+        // aber irgendwas ist falsch vllt mit OnCreate lifecycle
+
+    }
 }
